9b0a778077ca7ed963d2b0fc6b0dedbc8a87948a
zolppy <zolppy.me@gmail.com>
2026-06-10 08:17:45 -0300

    feat: novos tipos de autenticação

--- types/authentication.type.ts
+++ types/authentication.type.ts
@@ -1,6 +1,8 @@
 import z from "zod";
-import { credInSchema } from "@/schemas/authentication.schema";
+import { credInSchema, tokenOutSchema } from "@/schemas/authentication.schema";
 
 type CredIn = z.infer<typeof credInSchema>;
 
-export type { CredIn };
+type TokenOut = z.infer<typeof tokenOutSchema>;
+
+export type { CredIn, TokenOut };
