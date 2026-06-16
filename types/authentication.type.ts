15dd96665753514f4483180eac7f6b9ae3196120
zolppy <zolppy.me@gmail.com>
2026-06-16 15:52:20 -0300

    style: conveniências de programação

--- types/authentication.type.ts
+++ types/authentication.type.ts
@@ -2,7 +2,6 @@ import z from "zod";
 import { credInSchema, tokenOutSchema } from "@/schemas/authentication.schema";
 
 type CredIn = z.infer<typeof credInSchema>;
-
 type TokenOut = z.infer<typeof tokenOutSchema>;
 
 export type { CredIn, TokenOut };
